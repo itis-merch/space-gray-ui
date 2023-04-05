2ae1950b419b46620fbcbf92de8ca7372966226b
vitterre <bagatovbb@gmail.com>
2023-04-05 08:57:02 +0300

    style: format code

--- src/components/SGButton.vue
+++ src/components/SGButton.vue
@@ -2,16 +2,7 @@
 
 <template>
   <button
-    class="
-    bg-yellow-300
-    hover:bg-yellow-400
-    active:bg-yellow-300
-    text-stone-900
-    py-2
-    px-6
-    transition
-    duration-300
-    rounded"
+    class="bg-yellow-300 hover:bg-yellow-400 active:bg-yellow-300 text-stone-900 py-2 px-6 transition duration-300 rounded"
   >
     <slot />
   </button>
@@ -19,6 +10,6 @@
 
 <style scoped>
 button {
-  font-family: "VkSansDisplay-Bold";
+  font-family: 'VkSansDisplay-Bold';
 }
 </style>
