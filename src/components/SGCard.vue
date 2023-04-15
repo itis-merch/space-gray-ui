34b73d5dde59f921aa86fc76ab99dd208db20384
timergOoOt <timur18092004@gmail.com>
2023-04-15 21:41:57 +0300

    refactor: change colors of card's elements

--- src/components/SGCard.vue
+++ src/components/SGCard.vue
@@ -2,12 +2,12 @@
 
 <template>
 	<card class="">
-		<div class="bg-stone-300 w-52 h-68 m-5 py-2 rounded hover:bg-stone-400 transition duration-700 ">
+		<div class="bg-stone-200 w-52 h-68 m-5 py-2 rounded hover:bg-stone-50 active:bg-stone-50 transition duration-700 ">
 			<div class="bg-white w-48 h-44 mr-2 ml-2 rounded p-12"> Our picture</div>
 			<div class="text-stone-950 text-center">
 				<h2 class="text-lg sg-logo-text pt-1">TITLE</h2>
 				<h2 class="sg-logo-text pb-2">$999</h2>
-				<div class="sg-nav-linkfont-extralight text-base hover:text-white">Description</div>
+				<div class="sg-nav-link font-extralight text-sm hover:text-stone-600 active:text-stone-500">Description</div>
 			</div>
 		</div>
 	</card>
