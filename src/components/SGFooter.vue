6a7e85955407b453f46003fe539380099b424504
timergOoOt <timur18092004@gmail.com>
2023-04-15 21:22:45 +0300

    refactor: сhange the position of the columns

--- src/components/SGFooter.vue
+++ src/components/SGFooter.vue
@@ -5,8 +5,8 @@
 <template>
 	<footer class="bg-white">
 		<div class="mx-auto w-full max-w-screen2xl font-extralight text-gray-600 sg-nav-link text-base">
-			<div class="grid grid-cols-4 gap-x-24 px-16 py-6 lg:py-8 md:grid-cols-3">
-				<div>
+			<div class="grid grid-cols-3 gap-x-24 px-16 py-6 lg:py-8 md:grid-cols-3">
+				<div class="text-left">
 					<h3 class="mb-3 text-lg font-bold text-stone-950">More</h3>
 					<ul class="">
 						<li class="mb-2">
@@ -17,7 +17,7 @@
 						</li>
 					</ul>
 				</div>
-				<div>
+				<div class="text-center">
 					<h3 class="mb-3 text-lg font-bold text-stone-950">Contacts</h3>
 					<ul class="">
 						<li class="mb-2">
@@ -28,7 +28,7 @@
 						</li>
 					</ul>
 				</div>
-				<div>
+				<div class="text-right">
 					<h3 class="mb-3 text-lg font-bold text-stone-950">Contribute</h3>
 					<ul class="">
 						<li class="mb-2">
@@ -43,3 +43,9 @@
 		</div>
 	</footer>
 </template>
+
+<style scoped>
+footer {
+  padding-right: 20%;
+}
+</style>
