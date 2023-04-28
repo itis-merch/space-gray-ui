3fa3fc0532111a8cc4618b2141f1ba97c9c60d51
vitterre <bagatovbb@gmail.com>
2023-04-28 17:10:20 +0300

    style: move from spaces to tabs

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,107 +1,107 @@
 <template>
-    <div class="carousel">
-        <div class="carousel-inner">
-            <carousel-indicators
-                    :total="slides.length"
-                    :current-index="currentSlide"
-                    @switch="switchSlide($event)"
-            ></carousel-indicators>
-            <carousel-item
-                    v-for="(slide, index) in slides"
-                    :slide="slide"
-                    :key="`item-${index}`"
-                    :current-slide="currentSlide"
-                    :index="index"
-                    :direction="direction"
-            >
-            </carousel-item>
-        </div>
-    </div>
+	<div class="carousel">
+		<div class="carousel-inner">
+			<carousel-indicators
+					:total="slides.length"
+					:current-index="currentSlide"
+					@switch="switchSlide($event)"
+			></carousel-indicators>
+			<carousel-item
+					v-for="(slide, index) in slides"
+					:slide="slide"
+					:key="`item-${index}`"
+					:current-slide="currentSlide"
+					:index="index"
+					:direction="direction"
+			>
+			</carousel-item>
+		</div>
+	</div>
 </template>
 
 <script>
 import CarouselItem from "@/components/CarouselItem.vue";
 import CarouselIndicators from "@/components/CarouselIndicators.vue";
 export default {
-    components: {
-        CarouselItem,
-        CarouselIndicators
-    },
-    props: {
-        slides: {
-            type: Array,
-            required: true
-        }
-    },
-    data () {
-        return {
-            interval: 7000,
-            currentSlide: 0,
-            slideInterval: null,
-            direction: "right",
-        }
-    },
-    methods: {
-        setCurrentSlide(index) {
-            this.currentSlide = index;
-        },
-        prev(step = -1) {
-            const index =
-                this.currentSlide > 0
-                    ? this.currentSlide + step
-                    : this.slides.length - 1;
-            this.setCurrentSlide(index);
-            this.direction = "left";
-            this.startSlideTimer();
-        },
-        _next(step = 1) {
-            const index =
-                this.currentSlide < this.slides.length - 1
-                    ? this.currentSlide + step
-                    : 0;
-            this.setCurrentSlide(index);
-            this.direction = "right";
-        },
-        next(step = 1) {
-            this._next(step);
-            this.startSlideTimer();
-        },
-        startSlideTimer() {
-            this.stopSlideTimer();
-            this.slideInterval = setInterval(() => {
-                this._next();
-            }, this.interval);
-        },
-        stopSlideTimer() {
-            clearInterval(this.slideInterval);
-        },
-        switchSlide(index) {
-            const step = index - this.currentSlide;
-            if (step > 0) {
-                this.next(step);
-            } else if (step < 0) {
-                this.prev(step);
-            }
-        },
-    },
-    mounted() {
-        this.startSlideTimer();
-    },
-    beforeUnmount() {
-        this.stopSlideTimer();
-    },
+	components: {
+		CarouselItem,
+		CarouselIndicators
+	},
+	props: {
+		slides: {
+			type: Array,
+			required: true
+		}
+	},
+	data () {
+		return {
+			interval: 7000,
+			currentSlide: 0,
+			slideInterval: null,
+			direction: "right",
+		}
+	},
+	methods: {
+		setCurrentSlide(index) {
+			this.currentSlide = index;
+		},
+		prev(step = -1) {
+			const index =
+				this.currentSlide > 0
+					? this.currentSlide + step
+					: this.slides.length - 1;
+			this.setCurrentSlide(index);
+			this.direction = "left";
+			this.startSlideTimer();
+		},
+		_next(step = 1) {
+			const index =
+				this.currentSlide < this.slides.length - 1
+					? this.currentSlide + step
+					: 0;
+			this.setCurrentSlide(index);
+			this.direction = "right";
+		},
+		next(step = 1) {
+			this._next(step);
+			this.startSlideTimer();
+		},
+		startSlideTimer() {
+			this.stopSlideTimer();
+			this.slideInterval = setInterval(() => {
+				this._next();
+			}, this.interval);
+		},
+		stopSlideTimer() {
+			clearInterval(this.slideInterval);
+		},
+		switchSlide(index) {
+			const step = index - this.currentSlide;
+			if (step > 0) {
+				this.next(step);
+			} else if (step < 0) {
+				this.prev(step);
+			}
+		},
+	},
+	mounted() {
+		this.startSlideTimer();
+	},
+	beforeUnmount() {
+		this.stopSlideTimer();
+	},
 }
 </script>
 
 <style scoped>
 .carousel {
-    display: flex;
-    justify-content: center;
+	display: flex;
+	justify-content: center;
 }
 .carousel-inner {
-    position: relative;
-    width: 500px;
-    height: 280px;
-    overflow: hidden;
+	position: relative;
+	width: 500px;
+	height: 280px;
+	overflow: hidden;
 }
 </style>
--- src/components/CarouselIndicators.vue
+++ src/components/CarouselIndicators.vue
@@ -1,49 +1,49 @@
 <template>
-    <div class="carousel-indicators">
-        <button
-                class="carousel-indicator-item"
-                :class="{active: currentIndex === index}"
-                v-for="(item, index) in total"
-                :key="index"
-                @click="$emit('switch', index)"
-        ></button>
-    </div>
+	<div class="carousel-indicators">
+		<button
+				class="carousel-indicator-item"
+				:class="{active: currentIndex === index}"
+				v-for="(item, index) in total"
+				:key="index"
+				@click="$emit('switch', index)"
+		></button>
+	</div>
 </template>
 
 <script>
 export default {
-    emits: ["switch"],
-    props: {
-        total: {
-            type: Number,
-        },
-        currentIndex: {
-            type: Number,
-        }
-    }
+	emits: ["switch"],
+	props: {
+		total: {
+			type: Number,
+		},
+		currentIndex: {
+			type: Number,
+		}
+	}
 }
 </script>
 
 <style scoped>
 .carousel-indicators {
-    position: absolute;
-    transform: translateX(-50%);
-    left: 50%;
-    bottom: -0.43em;
-    z-index: 2;
+	position: absolute;
+	transform: translateX(-50%);
+	left: 50%;
+	bottom: -0.43em;
+	z-index: 2;
 }
 .carousel-indicator-item {
-    padding: 0;
-    width: 7px;
-    height: 7px;
-    border: none;
-    background: #545454;
-    opacity: .5;
-    margin: 0.2em;
-    border-radius: 50%;
-    cursor: pointer;
+	padding: 0;
+	width: 7px;
+	height: 7px;
+	border: none;
+	background: #545454;
+	opacity: .5;
+	margin: 0.2em;
+	border-radius: 50%;
+	cursor: pointer;
 }
 .active {
-    opacity: 1;
+	opacity: 1;
 }
 </style>
