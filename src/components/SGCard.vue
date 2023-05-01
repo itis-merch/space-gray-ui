770339a7e04ef521c5cae5846aa9eb0feba0e32e
timergOoOt <timur18092004@gmail.com>
2023-05-01 17:20:54 +0300

    fix: repositioning of text and photo

--- src/components/SGCard.vue
+++ src/components/SGCard.vue
@@ -3,11 +3,13 @@
 <template>
 	<card class="">
 		<div class="bg-stone-200 w-52 h-68 m-5 py-2 rounded hover:bg-stone-50 active:bg-stone-50 transition duration-700 ">
-			<div class="bg-white w-48 h-44 mr-2 ml-2 rounded p-12"> Our picture</div>
+			<div class="bg-white w-48 h-44 mr-2 ml-2 rounded"><img class="object-cover bg-no-repeat bg-center w-full h-full rounded" src="../assets/images/test_img.jpg"  alt=""></div>
 			<div class="text-stone-950 text-center">
-				<h2 class="text-lg sg-logo-text pt-1">TITLE</h2>
-				<h2 class="sg-logo-text pb-2">$999</h2>
-				<div class="sg-nav-link font-extralight text-sm hover:text-stone-600 active:text-stone-500">Description</div>
+				<div class="sg-logo-text text-lg clear-both mx-3">
+					<h2 class="float-left">TITLE</h2>
+					<h2 class="float-right">$999</h2>
+				</div>	
+				<div class="sg-nav-link font-extralight text-sm hover:text-stone-600 active:text-stone-500 text-justify mx-3">Description kgfvv dd  fvhifbvifvf fvigfvifbvifbv fvbfivgfvifgvfibvvif</div>
 			</div>
 		</div>
 	</card>
